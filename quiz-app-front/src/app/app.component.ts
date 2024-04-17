dee18fbf8d998f7fe627c2613259a82cb366e6cd
Saori Kojima <skojima@atu.edu>
2024-04-17 16:10:49 -0500

    added routes

--- quiz-app-front/src/app/app.component.ts
+++ quiz-app-front/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'quiz-app-front';
+  title = 'Trivia Quiz';
 }
--- quiz-app-front/src/app/app.routes.ts
+++ quiz-app-front/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Page Components
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { QuizSelectPageComponent } from './quiz-select-page/quiz-select-page.component';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
+import { ResultPageComponent } from './result-page/result-page.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'quiz-select', component: QuizSelectPageComponent },
+  { path: 'quiz', component: QuizPageComponent },
+  { path: 'result', component: ResultPageComponent },
+];
