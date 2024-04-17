1ea0a44dc58afd03c5501116646aaac9791c6694
Saori Kojima <skojima@atu.edu>
2024-04-17 15:49:58 -0500

    created page components

---
+++ quiz-app-front/src/app/quiz-select-page/quiz-select-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-select-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-select-page.component.html',
+  styleUrl: './quiz-select-page.component.css'
+})
+export class QuizSelectPageComponent {
+
+}
